a55bfb4eb8a0d7307e46b50d2127bdc0d9bffe35
Carter Jones <carter@carterjones.info>
2017-04-01 01:53:49 -0700

    use release folder

--- nix/connect-scripts-dir.sh
+++ nix/connect-scripts-dir.sh
@@ -6,15 +6,16 @@ git_root=$(git rev-parse --show-toplevel)
 # Set up the scripts directory for both users.
 for user in "shad0wfyr3" "dr_dvorak" ; do
 
+    dst="${git_root}/release"
     user_dir="$HOME/.config/hackmud/${user}/scripts"
     mkdir -p $user_dir
 
     echo 'Linking $src to $dst:'
-    echo "  [src] ${git_root}"
+    echo "  [src] ${dst}"
     echo "  [dst] ${user_dir}"
 
     rm -rf $user_dir
-    ln -s "${git_root}" "${user_dir}"
+    ln -s "${dst}" "${user_dir}"
 
     echo "Done."
 
